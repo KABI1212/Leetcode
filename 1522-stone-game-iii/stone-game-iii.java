37f276cebe34d0a867b4d40aeb4d5da9f362b250
KABILESH <kabileshk702@gmail.com>
2026-08-03 07:09:02 +0530

    Time: 10 ms (83.24%) | Memory: 85.6 MB (65.91%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String stoneGameIII(int[] s) {
+        int n = s.length;
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+            for (int j = i; j < Math.min(i + 3, n); j++) {
+                sum += s[j];
+                dp[i] = Math.max(dp[i], sum - dp[j + 1]);
+            }
+        }
+        if (dp[0] > 0) {
+            return "Alice";
+        } else if (dp[0] < 0) {
+            return "Bob";
+        } else {
+            return "Tie";
+        }
+    }
+}
