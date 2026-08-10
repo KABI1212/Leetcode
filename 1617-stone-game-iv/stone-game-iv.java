a165b50c1f2ed6584f85c5e026d40b6d5f7fb080
KABILESH <kabileshk702@gmail.com>
2026-08-10 07:05:24 +0530

    Time: 14 ms (91.38%) | Memory: 42.6 MB (68.41%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,10 +1,7 @@
 class Solution {
-
-    static final int MAX = 100000;
-    static final boolean[] dp = new boolean[MAX + 1];
-
-    static {
-        for (int i = 1; i <= MAX; i++) {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
                 if (!dp[i - j * j]) {
                     dp[i] = true;
@@ -12,8 +9,6 @@ class Solution {
                 }
             }
         }
-    }
-    public boolean winnerSquareGame(int n) {
         return dp[n];
     }
 }
